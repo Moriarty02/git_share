07b45f718c00f19e54355f5043e7b512af1507fb
zhangxiuhua <837001764@qq.com>
2015-09-30 22:30:51 +0800

    misc add new

---
+++ 算法和脑残值/find_K_MAX.js
@@ -0,0 +1,27 @@
+//找到一个数组中第k大的数字(内部不重复)
+//第一个思路，也是拿到这个问题马上可以想到的方法
+//思考问题的时候，我喜欢结合已经学过的知识或者生活场景来切入问题
+//如何去找到第K大的数字，我们学过的最简单的排序方法是什么？冒泡吧，
+//冒泡的思路就是每次将最大的数字冒泡上来与第一个数字交换，
+//放到这里，我们只需要第K大的数字，实际就是冒泡第K次的数字
+//这样就比较简单了吧
+
+
+//还是先复习一下冒泡吧
+function bubble(a) {
+ console.log(a);
+ var  arr=a.split("");
+ var flag=true;
+ for(var i =0,length=arr.length;i<length;i++){
+ 	for(var j=length-1;j>i;j--){
+ 		if(arr[])
+ 	}
+
+ }
+
+}
+
+function find_K_MAX(a, low, high, k) {
+
+
+}
